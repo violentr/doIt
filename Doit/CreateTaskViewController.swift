9eb45dfc3d48c6a342601d2875f27644d9397b6b
Deniss <den.b@itservicesgroup.co.uk>
2018-01-01 23:40:50 +0000

    Add task to the tasks list and reload data to show it

--- Doit/CreateTaskViewController.swift
+++ Doit/CreateTaskViewController.swift
@@ -26,8 +26,8 @@ class CreateTaskViewController: UIViewController {
      task.name = taskNameTextField.text!
      task.important = importantSwitch.isOn
      previousVC.tasks.append(task)
+     previousVC.taskList.reloadData()
   }
-  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
