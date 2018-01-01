776fb0541c62c039553fd3f7e979e4ba4605d4bc
Deniss <den.b@itservicesgroup.co.uk>
2018-01-01 14:09:08 +0000

    Add TableView with 10 rows of string 'Hello'

--- Doit/ViewController.swift
+++ Doit/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,  UITableViewDataSource, UITableViewDelegate {
 
+  @IBOutlet weak var taskList: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    taskList.dataSource = self
+    taskList.delegate = self
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10;
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel?.text = "hello"
+    return cell
+    
+  }
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
